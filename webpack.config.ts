f543951f0656bb7bc90901ee6acc70a33104656e
DmitryB89 <dbajdarov89@gmail.com>
2025-01-25 17:17:57 +0300

    fix(webpack): добавление ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,8 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-const {nodeModules} = require("ts-loader/dist/constants");
+import path from 'path'
+import webpack from 'webpack'
+import HTMLWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
+const config: webpack.Configuration = {
     mode: 'production',
     entry:
         path.resolve(__dirname, 'src', 'index.ts'),
@@ -17,10 +16,11 @@ module.exports = {
             template: path.resolve(__dirname, 'public', 'index.html'),
         }),
         new webpack.ProgressPlugin(),
-        ],
+    ],
     module: {
         rules: [
             {
+                //Файлы, необходимые пропустить через loader (loader необходим для обработки файлов, выходящих за рамки js)
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
@@ -32,3 +32,5 @@ module.exports = {
         extensions: ['tsx', '.ts', '.js']
     }
 }
+
+export default config
