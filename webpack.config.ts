15753283e5c99469ec14d1287865605eb3c51eb4
DmitryB89 <dbajdarov89@gmail.com>
2025-02-07 21:09:42 +0300

    refactor(config): декомпозиция конфига

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,36 +1,22 @@
 import path from 'path'
 import webpack from 'webpack'
-import HTMLWebpackPlugin from 'html-webpack-plugin';
 
-const config: webpack.Configuration = {
-    mode: 'production',
-    entry:
-        path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, 'build'),
-        clean: true
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html'),
-        }),
-        new webpack.ProgressPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                //Файлы, необходимые пропустить через loader (loader необходим для обработки файлов, выходящих за рамки js)
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    resolve: {
-        //чтобы не писать расширение файла при импорте
-        extensions: ['tsx', '.ts', '.js']
-    }
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildPaths} from "./config/build/types/config";
+
+const mode = 'development';
+const isDev = mode === 'development';
+
+const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build', 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html')
 }
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: 'development',
+    paths: paths,
+    isDev
+})
 
 export default config
