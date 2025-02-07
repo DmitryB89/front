e0908d36650c87aeeb17031084c1604e4849838f
DmitryB89 <dbajdarov89@gmail.com>
2025-02-07 22:14:50 +0300

    feat(webpack): добавление env

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 import {someFn} from "./test";
 someFn()
+document.body.innerHTML = `<div style='background:red'>Hello world</div>`
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,21 +2,27 @@ import path from 'path'
 import webpack from 'webpack'
 
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/config";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
 
-const mode = 'development';
-const isDev = mode === 'development';
 
-const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'build', 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html')
+
+export default (env: BuildEnv) => {
+    const mode = env.mode || 'development';
+    const isDev = mode === 'development';
+
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname, 'build', 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html')
+    }
+
+    const PORT = env.port || 3000;
+
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode,
+        paths: paths,
+        isDev,
+        port: PORT
+    })
+    return config
 }
-
-const config: webpack.Configuration = buildWebpackConfig({
-    mode: 'development',
-    paths: paths,
-    isDev
-})
-
-export default config
